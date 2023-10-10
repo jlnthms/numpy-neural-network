9a48981ab7d2d8679162f4f3892ac8203fdc69be
Julien THOMAS <thomasjulien92140@gmail.com>
2023-10-11 00:41:37 +0200

    momentum added to SGD
    should it be a parent attribute ?

--- NeuralNetwork/optimizer.py
+++ NeuralNetwork/optimizer.py
@@ -40,17 +40,35 @@ class SGD(Optimizer):
     SGD aims to estimate the "direction" towards minimum training loss using its derivative,
     and update weights and biases in consequence.
     """
-    def __init__(self, learning_rate):
+    def __init__(self, learning_rate, momentum=None):
         super().__init__(learning_rate)
+        self.momentum = momentum
+        self.velocities = {}
 
     def step(self, layers: List[Layer], grads_w, grads_b):
         for layer, grad_w, grad_b in zip(layers[1:], grads_w, grads_b):
             for neuron, grad_weights, grad_bias in zip(layer.neurons, grad_w, grad_b):
-                # Update weights for each neuron in the layer
-                for i in range(len(neuron.weights)):
-                    neuron.weights[i] -= self.learning_rate * grad_weights[i]
+                # Check if the neuron has been encountered before, and initialize its velocity if not
+                if neuron not in self.velocities:
+                    self.velocities[neuron] = {
+                        'weights': [0.0] * len(neuron.weights),
+                        'bias': 0.0
+                    }
+                if self.momentum is not None:
+                    # Update weights for each neuron in the layer
+                    for i in range(len(neuron.weights)):
+                        velocity = self.momentum * self.velocities[neuron]['weights'][i] + grad_weights[i]
+                        neuron.weights[i] -= self.learning_rate * velocity
+                        self.velocities[neuron]['weights'][i] = velocity
 
-                # Update biases for each neuron in the layer
-                neuron.bias -= self.learning_rate * grad_bias
+                        # Update biases for each neuron in the layer with momentum
+                        velocity = self.momentum * self.velocities[neuron]['bias'] + grad_bias
+                        neuron.bias -= self.learning_rate * velocity
+                        self.velocities[neuron]['bias'] = velocity
+                else:
+                    # If momentum is not provided, perform regular SGD
+                    for i in range(len(neuron.weights)):
+                        neuron.weights[i] -= self.learning_rate * grad_weights[i]
+                    neuron.bias -= self.learning_rate * grad_bias
 
 
