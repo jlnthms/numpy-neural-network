0c7e4921dd205ebfdde2239635379912da34cb07
Julien THOMAS <thomasjulien92140@gmail.com>
2023-11-04 15:32:24 +0100

    implemented kernel he initatialization

--- CNN/network.py
+++ CNN/network.py
@@ -9,6 +9,14 @@ class ConvNeuralNet:
         self.layers = layers
         self.classifier = classifier
 
+    def he_initialization(self):
+        for i, layer in enumerate(self.layers):
+            if isinstance(layer, ConvLayer):
+                for kernel in layer.kernels:
+                    fan_in = len(self.layers[i-1].output)
+                    std_dev = np.sqrt(2.0 / fan_in)
+                    kernel.array = np.random.randn(*kernel.weights.shape) * std_dev
+
     def forward(self, image):
         self.layers[0].inputs.append(image)
         for layer in self.layers:
@@ -20,6 +28,11 @@ class ConvNeuralNet:
                 layer.flatten()
             else:
                 raise TypeError(f'Invalid or Incomplete layer definition: {layer} of type {type(layer)}')
+
+        self.classifier.forward(self.layers[-1].output[0])
+
+    def backward(self, label):
+        pass
 
 
 
