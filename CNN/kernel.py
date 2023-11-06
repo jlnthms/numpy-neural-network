a043ea8118e1bd05c427720062b97e1b2485ae58
Julien THOMAS <thomasjulien92140@gmail.com>
2023-11-06 23:41:29 +0100

    removed unnecessary derived classes

--- CNN/kernel.py
+++ CNN/kernel.py
@@ -19,30 +19,7 @@ class Kernel:
         for c in range(num_channels):
             for i in range(0, ih - kh + 1, stride):
                 for j in range(0, iw - kw + 1, stride):
-                    region = image[i:i+kh, j:j+kw, c]
-                    feature_map[i//stride, j//stride, c] = np.sum(self.array * region)
+                    region = image[i:i + kh, j:j + kw, c]
+                    feature_map[i // stride, j // stride, c] = np.sum(self.array * region)
 
         return feature_map
-
-
-class GaussianKernel(Kernel):
-    def __init__(self, sigma, size):
-        array = np.array([])
-        super().__init__(size)
-        self.sigma = sigma
-        # Create the Gaussian kernel array
-        kernel = np.fromfunction(
-            lambda x, y: (1 / (2 * np.pi * self.sigma ** 2)) * np.exp(
-                -((x - self.size[0] // 2) ** 2 + (y - self.size[1] // 2) ** 2) / (2 * self.sigma ** 2)),
-            self.size
-        )
-        self.array = kernel / np.sum(kernel)  # Normalize the kernel
-
-
-class EdgeDetectionKernel(Kernel):
-    def __init__(self, size):
-        super().__init__(size)
-        # Define the kernel elements for edge detection
-        self.array = np.array([[-1, -1, -1],
-                                    [-1, 8, -1],
-                                    [-1, -1, -1]], dtype=np.int32)
