f00304aa6679f659a28d6e060b48e3bba2fd2190
Julien THOMAS <thomasjulien92140@gmail.com>
2023-11-14 01:07:50 +0100

    added case of grayscale images in convolution
    single channel is ignored by numpy in the input causing "too many values to unpack" when declaring dimension variables.

--- CNN/kernel.py
+++ CNN/kernel.py
@@ -10,16 +10,27 @@ class Kernel:
 
     def convolve(self, image: np.ndarray, stride=1, padding=0):
         kh, kw = self.array.shape
-        ih, iw, num_channels = image.shape
+        if len(image.shape) == 3:
+            ih, iw, num_channels = image.shape
+        else:
+            ih, iw = image.shape
+            num_channels = 1
         ph, pw = padding, padding
         output_h = (ih - kh + 2 * ph) // stride + 1
         output_w = (iw - kw + 2 * pw) // stride + 1
-        feature_map = np.zeros((output_h, output_w, num_channels), dtype=image.dtype)
+        if num_channels == 1:
+            feature_map = np.zeros((output_h, output_w), dtype=image.dtype)
+        else:
+            feature_map = np.zeros((output_h, output_w, num_channels), dtype=image.dtype)
 
         for c in range(num_channels):
             for i in range(0, ih - kh + 1, stride):
                 for j in range(0, iw - kw + 1, stride):
-                    region = image[i:i + kh, j:j + kw, c]
-                    feature_map[i // stride, j // stride, c] = np.sum(self.array * region)
+                    if num_channels == 1:
+                        region = image[i:i + kh, j:j + kw]
+                        feature_map[i // stride, j // stride] = np.sum(self.array * region)
+                    else:
+                        region = image[i:i + kh, j:j + kw, c]
+                        feature_map[i // stride, j // stride, c] = np.sum(self.array * region)
 
         return feature_map
