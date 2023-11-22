0afb6aed50da75480061ef9e819c2e431ab7f339
Julien THOMAS <thomasjulien92140@gmail.com>
2023-11-23 00:46:04 +0100

    corrected backward pass

--- CNN/network.py
+++ CNN/network.py
@@ -2,6 +2,7 @@ from typing import List
 
 from CNN.layer import *
 from NeuralNetwork.network import MultiLayerPerceptron
+from scipy.signal import convolve2d
 
 
 class ConvNeuralNet:
@@ -22,11 +23,13 @@ class ConvNeuralNet:
                     kernel.array = np.random.randn(*kernel.array.shape) * std_dev
                     output_size = layer.size
 
+        self.classifier.he_init()
+
     def forward(self, image):
         self.layers[0].inputs.append(image)
         for i, layer in enumerate(self.layers):
             if i != 0:
-                layer.inputs = self.layers[i-1].output
+                layer.inputs = self.layers[i - 1].output
             if isinstance(layer, ConvLayer):
                 layer.convolve()
                 layer.activate()
@@ -42,6 +45,7 @@ class ConvNeuralNet:
     def backward(self, label):
         # Compute the gradient of the loss with respect to the output of the classifier
         grad_wrt_outputs = self.classifier.backward(label)[2]
+        grad_wrt_outputs = np.dot(grad_wrt_outputs, self.classifier.layers[1].get_weights()) * 1
 
         # Unflatten grad_wrt_outputs to the shape of the last layer's output
         grad_wrt_outputs = grad_wrt_outputs.reshape(np.array(self.layers[-1].inputs).shape)
@@ -52,6 +56,7 @@ class ConvNeuralNet:
                 # If it's a ConvLayer, we need to compute gradients for kernels and back-propagate the error
                 # Create an empty list to store gradients for each kernel in this layer
                 kernel_gradients = [np.zeros_like(kernel.array) for kernel in layer.kernels]
+                new_gradient_wrt_outputs = np.zeros_like(layer.inputs)
                 # Iterate over the feature maps in the current layer
                 for i in range(len(layer.output)):
                     # Compute the gradients for this feature map
@@ -59,36 +64,40 @@ class ConvNeuralNet:
                     # Back-propagate through the activation function
                     gradient = gradient * layer.activation(layer.output[i], derivative=True)
                     # Update kernel gradients using convolution with the corresponding input
-                    for j in range(len(layer.kernels)):
-                        input_map = layer.inputs[i]
-                        kernel_gradients[j] += np.rot90(np.rot90(np.convolve(input_map, gradient, 'valid')))
-                    # Compute the gradient for the input feature map for the next layer
-                    grad_wrt_outputs[i] = np.zeros_like(grad_wrt_outputs[i])
-                    for j in range(len(layer.kernels)):
-                        grad_wrt_outputs[i] += np.convolve(layer.kernels[j].array, gradient, 'full')
+                    for j, input_map in enumerate(layer.inputs):
+                        kernel_gradients[i] += convolve2d(input_map, gradient, 'valid')
+                        # Compute the gradient for the input feature map for the next layer
+                        new_gradient_wrt_outputs[j] += convolve2d(gradient, np.rot90(np.rot90(layer.kernels[i].array)), 'full')
 
+                grad_wrt_outputs = new_gradient_wrt_outputs
                 grads_k.insert(0, kernel_gradients)
 
             if isinstance(layer, PoolLayer):
-                if layer.method == 'max':
-                    gradient_map = np.zeros_like(layer.inputs[0])
-                    for i in range(len(layer.output)):
-                        for j in range(len(layer.output[i])):
-                            for k in range(len(layer.output[i][j])):
-                                region = layer.inputs[i][j * layer.stride:(j * layer.stride) + layer.pool_shape[0],
-                                         k * layer.stride:(k * layer.stride) + layer.pool_shape[1]]
-                                max_val = np.max(region)
-                                gradient_map[j * layer.stride:(j * layer.stride) + layer.pool_shape[0],
-                                k * layer.stride:(k * layer.stride) + layer.pool_shape[1]] = (layer.output[i][j][k] == max_val) * grad_wrt_outputs[i][j][k]
-                    grad_wrt_outputs = gradient_map
+                gradient_maps = np.zeros_like(layer.inputs)
 
-                elif layer.method == 'average':
-                    gradient_map = np.zeros_like(layer.inputs[0])
-                    for i in range(len(layer.output)):
-                        for j in range(len(layer.output[i])):
-                            for k in range(len(layer.output[i][j])):
-                                gradient_map[j * layer.stride:(j * layer.stride) + layer.pool_shape[0],
-                                k * layer.stride:(k * layer.stride) + layer.pool_shape[1]] += grad_wrt_outputs[i][j][k] / (layer.pool_shape[0] * layer.pool_shape[1])
-                    grad_wrt_outputs = gradient_map
+                for i in range(len(layer.inputs)):
+                    input_map = layer.inputs[i]
+                    grad_wrt_output = grad_wrt_outputs[i]
 
+                    for h in range(0, input_map.shape[0], layer.stride):
+                        for w in range(0, input_map.shape[1], layer.stride):
+                            if layer.method == 'max':
+                                # Find the index of the maximum value in the corresponding pooling region
+                                max_index = np.unravel_index(
+                                    np.argmax(input_map[h:h + layer.pool_shape[0], w:w + layer.pool_shape[1]]), layer.pool_shape)
+                                if len(input_map.shape) > 2:
+                                    for c in range(input_map.shape[2]):
+                                        gradient_maps[i][h + max_index[0], w + max_index[1], c] = grad_wrt_output[h // layer.stride, w // layer.stride, c]
+                                else:
+                                    gradient_maps[i][h + max_index[0], w + max_index[1]] = grad_wrt_output[h // layer.stride, w // layer.stride]
+
+                            elif layer.method == 'average':
+                                if len(input_map.shape) > 2:
+                                    for c in range(input_map.shape[2]):
+                                        gradient_maps[i][h:h + layer.pool_shape[0], w:w + layer.pool_shape[1], c] += \
+                                            grad_wrt_output[h // layer.stride, w // layer.stride, c] / (layer.pool_shape[0] * layer.pool_shape[1])
+                                else:
+                                    gradient_maps[i][h:h + layer.pool_shape[0], w:w + layer.pool_shape[1]] += \
+                                        grad_wrt_output[h // layer.stride, w // layer.stride] / (layer.pool_shape[0] * layer.pool_shape[1])
+                grad_wrt_outputs = gradient_maps
         return grads_k
