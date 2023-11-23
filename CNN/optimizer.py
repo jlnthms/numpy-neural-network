6dea5163cac7b9f73ffebe1435f2c067f443c7d5
Julien THOMAS <thomasjulien92140@gmail.com>
2023-11-23 11:49:35 +0100

    added classifier optimization

--- CNN/optimizer.py
+++ CNN/optimizer.py
@@ -1,22 +1,25 @@
 from typing import List
 
 from CNN.layer import *
+from NeuralNetwork.optimizer import SGD as NnSGD
 
 
 class Optimizer:
     def __init__(self, learning_rate):
         self.learning_rate = learning_rate
 
-    def step(self, layers: List[Layer], grads_k):
+    def step(self, classifier, layers: List[Layer], grad_class_w, grad_class_b, grads_k):
         pass
 
 
 class SGD(Optimizer):
     def __init__(self, learning_rate):
         super().__init__(learning_rate)
-        pass
+        self.classifier_optimizer = NnSGD(learning_rate)
 
-    def step(self, layers: List[Layer], grads_k):
+    def step(self, classifier, layers: List[Layer], grad_class_w, grad_class_b, grads_k):
+        self.classifier_optimizer.step(classifier.layers, grad_class_w, grad_class_b)
         for layer, kernel_grads in zip(layers, grads_k):
-            for kernel, kernel_grad in zip(layer.kernels, kernel_grads):
-                kernel.array -= self.learning_rate * kernel_grad
+            if isinstance(layer, ConvLayer):
+                for kernel, kernel_grad in zip(layer.kernels, kernel_grads):
+                    kernel.array -= self.learning_rate * kernel_grad
