011ec5c89ab22e866744e614f1e36af107e50f17
Julien THOMAS <thomasjulien92140@gmail.com>
2023-11-23 19:02:57 +0100

    use np.multiply instead of * operator

--- CNN/kernel.py
+++ CNN/kernel.py
@@ -8,6 +8,8 @@ class Kernel:
         self.size = size
         self.array = np.zeros(size)
 
+    import numpy as np
+
     def convolve(self, image: np.ndarray, stride=1, padding=0):
         kh, kw = self.array.shape
         if len(image.shape) == 3:
@@ -18,19 +20,20 @@ class Kernel:
         ph, pw = padding, padding
         output_h = (ih - kh + 2 * ph) // stride + 1
         output_w = (iw - kw + 2 * pw) // stride + 1
+
         if num_channels == 1:
             feature_map = np.zeros((output_h, output_w), dtype=image.dtype)
         else:
             feature_map = np.zeros((output_h, output_w, num_channels), dtype=image.dtype)
 
-        for c in range(num_channels):
-            for i in range(0, ih - kh + 1, stride):
-                for j in range(0, iw - kw + 1, stride):
-                    if num_channels == 1:
-                        region = image[i:i + kh, j:j + kw]
-                        feature_map[i // stride, j // stride] = np.sum(self.array * region)
-                    else:
-                        region = image[i:i + kh, j:j + kw, c]
-                        feature_map[i // stride, j // stride, c] = np.sum(self.array * region)
+        for i in range(0, ih - kh + 1, stride):
+            for j in range(0, iw - kw + 1, stride):
+                if num_channels == 1:
+                    region = image[i:i + kh, j:j + kw]
+                    feature_map[i // stride, j // stride] = np.sum(np.multiply(self.array, region))
+                else:
+                    region = image[i:i + kh, j:j + kw, :]
+                    feature_map[i // stride, j // stride, :] = np.sum(np.multiply(self.array, region), axis=(0, 1))
 
         return feature_map
+
