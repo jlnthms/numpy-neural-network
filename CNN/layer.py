cdf1898489749b7ba36380af6f43a828d02f865b
Julien THOMAS <thomasjulien92140@gmail.com>
2023-11-23 11:48:39 +0100

    forgot to clear the outputs between fwd passes

--- CNN/layer.py
+++ CNN/layer.py
@@ -30,6 +30,9 @@ class ConvLayer(Layer):
             raise ValueError("Invalid activation function name")
 
     def convolve(self, stride=1, padding=0):
+        # clear output from previous pass
+        if self.output:
+            self.output = []
         for kernel in self.kernels:
             feature_maps = []
             for image in self.inputs:
@@ -52,6 +55,9 @@ class PoolLayer(Layer):
         self.method = method
 
     def pool(self):
+        # clear output from previous pass
+        if self.output:
+            self.output = []
         if self.method not in ['max', 'average']:
             raise ValueError(f'Non-valid Pooling method: {self.method}')
 
@@ -91,4 +97,6 @@ class FlatLayer(Layer):
 
     def flatten(self):
         classifier_input = [feature_map.flatten() for feature_map in self.inputs]
+        if self.output:
+            self.output.pop()
         self.output.append(np.concatenate(classifier_input))
