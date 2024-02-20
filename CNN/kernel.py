11e5714438ab49f3407c751fa43e55ed83e1f862
Julien THOMAS <thomasjulien92140@gmail.com>
2024-02-20 18:13:57 +0100

    added full documentation for kernel.py

--- CNN/kernel.py
+++ CNN/kernel.py
@@ -1,9 +1,29 @@
+"""
+Kernel
+
+This module contains the base Kernel class
+
+Author: Julien THOMAS
+Date: November 04, 2023
+"""
+
 from typing import Tuple
 
 import numpy as np
 
 
 class Kernel:
+    """
+    A kernel is a filter, e.g a matrix of a defined size that is used to extract
+    features at different scales on an image.
+
+    Attributes:
+        size (Tuple[int, int]): height and width of the kernel.
+        array (np.ndarray): a matrix of shape 'size'.
+
+    Methods:
+        convolve(image, stride, padding): convolution of the kernel with an image.
+    """
     def __init__(self, size: Tuple[int, int]):
         self.size = size
         self.array = np.zeros(size)
@@ -11,6 +31,19 @@ class Kernel:
     import numpy as np
 
     def convolve(self, image: np.ndarray, stride=1, padding=0):
+        """
+        Kernel convolution over an image. The filter strides over the image and
+        element-wise multiplication is applied at every iteration between the kernel
+        array and the region of the image it is over.
+
+        Args:
+            image (np.ndarray): input image to be convolved with the kernel.
+            stride (int): number of pixels the kernel is shifted each iteration.
+            padding (int): number of lines added to each dimension in case kernel size does not divide image size.
+
+        Returns:
+             feature_map (np.ndarray): the input image after being convolved with the kernel.
+        """
         kh, kw = self.array.shape
         if len(image.shape) == 3:
             ih, iw, num_channels = image.shape
